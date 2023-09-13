5dc6c6d832a0183a86b0d08509ee29d27aaa5b31
Cristian Contreras <contreras.cristian994@gmail.com>
2023-09-13 15:34:08 -0400

    review form and section works perfectly

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,13 @@ import {Routes, Route} from 'react-router-dom';
 import Home from './components/home/Home';
 import Header from './components/header/Header';
 import Trailer from './components/trailer/Trailer';
+import Reviews from './components/reviews/Reviews';
 
 function App() {  
 
   const [movies, setMovies] = useState([]);
+  const [movie, setMovie] = useState([]);
+  const [reviews, setReviews] = useState([]);
 
   const getMovies = async () => {
 
@@ -25,6 +28,31 @@ function App() {
     }
   }
 
+  // const getMovieData = async (movieId) => {
+  //   try {
+  //       const response = await api.get(`/api/v1/movies/${movieId}`);
+  //       const singleMovie = response.data;
+  //       setMovie(singleMovie);
+  //       setReviews(singleMovie.reviews);
+  //   } catch (error) {
+  //       console.error(error);
+  //   }
+  // }
+
+  const getMovieData = async (movieId) => {
+
+    try {
+      const response = await api.get(`/api/v1/movies/${movieId}`);
+      const singleMovie = response.data;
+      setMovie(singleMovie);
+      setReviews(singleMovie.reviewIds);
+    }
+    catch (error) {
+      console.error(error);
+    }
+
+  }
+
   useEffect(() => {
     getMovies();
   }, [])
@@ -36,6 +64,7 @@ function App() {
         <Route path='/' element={<Layout />}>
           <Route path='/' element={<Home movies={ movies }/>}></Route>
           <Route path='/Trailer/:ytTrailerId' element={<Trailer />}></Route>
+          <Route path='/Reviews/:movieId' element={<Reviews getMovieData={getMovieData} movie={movie} reviews={reviews} setReviews={setReviews} />}></Route>
         </Route>
       </Routes>
     </div>
